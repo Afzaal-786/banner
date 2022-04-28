9d8ded889e28f66c385302b7b7ef8be5f5339cb8
Mr Cool <89538954+Afzaal-786@users.noreply.github.com>
2022-04-29 04:33:38 +0500

    Add files via upload

---
+++ requirement.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+clear
+apt update -y
+apt upgrade -y
+apt install figlet -y
+apt install lolcat -y
+apt install gem -y
+figlet -f big Basic Package Installation | lolcat
+apt install sl -y
+apt install toilet -y
+apt install cowsay -y
+apt install nano -y
+apt install ruby -y
+figlet Main
+sleep 1
+figlet Process
+sleep 1
+figlet is
+sleep 1
+figlet Coming
+sleep 1
+figlet Soon
+
+gem install lolcat
+figlet -f big Done !!! | lolcat
+echo
+echo -e "\e[1m Now Runing main process"
+echo
+echo -e "\e[44mCreated by Afzaal Khan (Mr_Cool)\e[0m"
+sleep 3
+sleep 1
+echo "loading."
+sleep 1
+echo "loading.."
+sleep 1
+echo "loading..."
+sleep 1
+echo "loading...."
+sleep 1
+echo "loading....."
+sleep 3
+bash banner.sh
